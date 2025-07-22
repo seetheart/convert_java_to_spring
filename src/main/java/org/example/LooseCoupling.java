c6e10ceff7ab7b7a81439c88cb7dedd160a08ccd
siddharthsingh <siddharthsiddhu899@gmail.com>
2025-07-22 09:59:35 +0530

    Challenge IOC

--- src/main/java/org/example/LooseCoupling.java
+++ src/main/java/org/example/LooseCoupling.java
@@ -1,10 +1,16 @@
 package org.example;
 
+import car.constructor.injection.Car;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class LooseCoupling {
     public static void main(String[] args) {
-        UserManager userManager = new UserManager(new UserDatabase());
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("challengeBeanContext.xml");
+        UserManager userManager = (UserManager) applicationContext.getBean("userManager");
+
         System.out.println(userManager.getUserDetails());
     }
 }
